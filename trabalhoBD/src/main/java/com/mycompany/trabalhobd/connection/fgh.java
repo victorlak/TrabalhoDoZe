5ad319ad4fc755a2214964e521d1fc38ff8cbb93
victor <133174073+victorlak@users.noreply.github.com>
2024-08-30 10:28:39 -0300

    Add files via upload

---
+++ trabalhoBD/src/main/java/com/mycompany/trabalhobd/connection/fgh.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhobd.connection;
+
+/**
+ *
+ * @author 14626232639
+ */
+public class fgh {
+    
+}
---
+++ trabalhoBD/src/main/java/com/mycompany/trabalhobd/model/entidades/Aluno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhobd.model.entidades;
+
+/**
+ *
+ * @author 14626232639
+ */
+public class Aluno {
+    
+}
